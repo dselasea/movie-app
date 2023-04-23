2e1b6bf9ddc5199e83ccb7b3622a87a2c873c1c0
Daniel <dselasea@gmail.com>
2023-04-23 21:43:24 +0000

    File and folder structure and components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import {Actors, MovieInformation, Movies, NavBar, Profile} from './';
+import useStyles from './styles'
 
 function App() {
-
+  const classes = useStyles();
   return (
-    <>
-      Hi there!
-    </>
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+        <main className={classes.content}>
+        <div className={classes.toolbar} />
+          <Routes>
+            <Route path='/movie/:id' element={<MovieInformation/>} />
+            <Route path='/actors/:id' element={<Actors />} />
+            <Route path='/' element={<Movies />} />
+            <Route path='/profile/:id' element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
